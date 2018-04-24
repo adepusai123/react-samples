be468fb5175494c26f83b0b73d2dc81f6351d37a
Sai Kumar <saikumara@wavelabs.in>
2018-04-24 19:33:44 +0530

    Challenge thing done

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,3 +12,19 @@ let me = new Person('Sai Kumar');
 console.log(me.getGreeting());
 let other = new Person();
 console.log(other.getGreeting());
+//challenge task
+// setup constructor to take name and age (default - 0)
+//get description - Sai is 25 years old!;
+
+class HelpMe {
+    constructor(name, age = 0){
+        this.name = name;
+        this.age = age;
+    }
+    getDescription(){
+        return `${this.name} is ${this.age} years old!`
+    }
+}
+
+let my = new HelpMe('Sai',25);
+console.log(my.getDescription());
