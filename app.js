83fb500889b83c58ae8a302a38bd08f2293ea82d
Sai Kumar <adepusai123@gmail.com>
2024-07-28 21:31:05 +0530

    JSX 1st syntax

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ import ReactDOM from 'react-dom';
 const heading = React.createElement("h1",{
     id: 'header'
 },"Hello World from React");
+// above code can be replaced by JSX code 
+const jsxHeader = <h1 id="header">This is JSX heading</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(jsxHeader);
