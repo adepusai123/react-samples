8b2c30b7b20c16b643b0efd86572447f548fe6d1
Sai Kumar <adepusai123@gmail.com>
2024-07-28 22:31:10 +0530

    React Element & Component

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; 
+import React from "react";
+import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1",{
-    id: 'header'
-},"Hello World from React");
-// above code can be replaced by JSX code 
-const jsxHeader = <h1 className="header">This is JSX heading</h1>;
+// React Element
+const heading = React.createElement(
+  "h1",
+  {
+    id: "header",
+  },
+  "Hello World from React"
+);
+// above code can be replaced by JSX code
+// for multiline JSX use () to wrap the code ex: (<h1>Hi</h1>)
+
+// React Element
+const jsxHeader = (
+  <div className="container">
+    <h1 className="header">This is JSX heading</h1>;
+  </div>
+);
+
+// React Component. Naming conversion should start with Upper case
+const HeaderComponent = ()=> {
+    return <div className="header">
+        <h1> Hello World! </h1>
+    </div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
