65ccb887849968f4f36de2ae3c471b39278ee98d
Sai Kumar <adepusai123@gmail.com>
2024-07-30 00:25:58 +0530

    Body and Restuarent card added

--- app.js
+++ app.js
@@ -18,25 +18,72 @@ import ReactDOM from "react-dom/client";
  *
  */
 const Header = () => {
-  return <div className="header">
-    <div >
-      <img  className="logo" src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg" alt="logo" />
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-list">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-    <div className="nav-list">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
+  );
 };
 
+const ResCard = () => {
+  return (
+    <div className="res-card">
+      <div>
+        <img
+          src="https://www.fabhotels.com/blog/wp-content/uploads/2018/08/Resize_290818_ABs.jpg"
+          alt="resturant"
+        />
+      </div>
+      <div className="info">Pandu Restuarant</div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="main-container">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h6>Copy Right text</h6>
+    </div>
+  );
+};
 const AppLayout = () => {
   return (
     <div className="layout">
       <Header />
+      <Body />
+      {Footer()}
     </div>
   );
 };
